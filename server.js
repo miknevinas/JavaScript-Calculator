f4ec578968794f9ef583e0d5009abec7781cda1a
Adam <miknevinas@gmail.com>
2016-03-28 14:48:56 -0600

    heroku commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-    res.writeHeat(200, {'Content-Type': 'text/html'});
-    res.end('');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello');
 })
 
-var port = Number(process.env.port || 3000);
-server.listen(port);
+var port = Number(process.env.PORT || 3000);
+server.listen(PORT);
