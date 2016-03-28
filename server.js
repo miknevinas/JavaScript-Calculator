bf08c131dbe649682f12de5eabbcd51a5e1eef1b
Adam <miknevinas@gmail.com>
2016-03-28 15:29:23 -0600

    heroku commit

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello');
+var port = process.env.PORT || 7000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+    res.render('index');
 })
 
-var port = Number(process.env.PORT || 3000);
-server.listen(PORT);
+app.listen(port, function() {
+    console.log('listening on port ' + port);
+})
